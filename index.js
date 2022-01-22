972e9fabae3be53a5dfce0dcb29136d6c0e9fdff
Abdel-2020 <abdelnoura97@gmail.com>
2022-01-22 18:05:49 +0000

    comments added, beautified, project complete.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
-
+//Share button function
 function shareButtonClicked() {
     //Find the share buttons and store them in the variable shareButton.
     let shareButton = document.querySelectorAll(".profile-share-button");
-
+//loop between both to check if they are clicked.
     for (i = 0; i < shareButton.length; i++) {
+        //if one is clicked, check if the social icons are showing. If not display them. 
         shareButton[i].addEventListener("click", e => {
             let socials = document.querySelector(".socials");
             if (socials.style.display == "none") {
@@ -12,9 +13,11 @@ function shareButtonClicked() {
             } else {
                 socials.style.display = "none";
             };
+            //I use this to debug when testing if it's registering button clicks. 
             console.log(e);
         })
     }
 };
 
+//Share button function call
 shareButtonClicked();
